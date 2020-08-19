7d2b274e8e4df16234fa3ff8b528d45a59cb7fa8
AleksMat <matej.aleksandrov@sinergise.com>
2020-08-19 15:58:18 +0200

    Added license file and removed almost all license headers

--- SentinelHub/dockwidget.py
+++ SentinelHub/dockwidget.py
@@ -1,21 +1,5 @@
 """
-/***************************************************************************
- SentinelHubDockWidget
-                             -------------------
-        begin                : 2017-07-07
-        copyright            : (C) 2020 by Sinergise
-        email                : info@sentinel-hub.com
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
- Module wrapping the widget interface
+Module wrapping the widget interface
 """
 
 import os
