33d63dda237af14934285c43f5be55191f604f5a
Adrián Di Paolo <46015264+Adrok24@users.noreply.github.com>
2023-03-02 12:10:49 +0100

    Add tests for sentinelhubdockwidget (#24)
    * Add tests for sentinelhubdockwidget
    
    *Add a fixture to initialize a QgsApplication
    
    *Add auxiliary function to test click a button
    
    * Add importorskip to the new tests
    
    * Move import inside the qgis_app fixture
    
    * Move method inside the test_widget module to avoid issues with CI and qgis libraries
    
    * Move sh_widget to another fixture

--- SentinelHub/tests/conftest.py
+++ SentinelHub/tests/conftest.py
@@ -11,3 +11,25 @@ INPUT_FOLDER = get_input_folder(__file__)
 @pytest.fixture(name="input_folder")
 def input_folder_fixture() -> str:
     return INPUT_FOLDER
+
+
+@pytest.fixture(scope="session")
+def qgis_app() -> None:
+    """Initialize a QgsApplication"""
+    from qgis.core import QgsApplication  # noqa: E402
+
+    qgis_app = QgsApplication([], True)
+    qgis_app.initQgis()
+
+    yield qgis_app
+    qgis_app.exitQgis()
+
+
+@pytest.fixture(scope="function")
+def sh_widget() -> None:
+    """Initialize the Sentinel Hub plugin widget"""
+    from ..dockwidget import SentinelHubDockWidget  # noqa: E402
+
+    sh_widget = SentinelHubDockWidget()
+    yield sh_widget
+    sh_widget.close()
