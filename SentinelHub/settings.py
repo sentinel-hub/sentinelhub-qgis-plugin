131d9d4c5df17285edc2c24e985e6116ce320625
Adrián Di Paolo <46015264+Adrok24@users.noreply.github.com>
2023-04-03 17:14:10 +0200

    Refactor Settings to inherit from QSettings (#25)
    * Refactor Settings to inherit from QSettings
    
    *Add typing
    
    *Add test cases for this class
    
    * include import in the fixture qsettings to avoid conflicts with the CI
    
    * Fix settings
    
    *remove auto_save
    
    *refactor copy method
    
    *Add test for copy method
    
    *Fix wrong typing
    
    * Fix scope in widget fixture
    
    * Fix type in settings copy method

--- SentinelHub/settings.py
+++ SentinelHub/settings.py
@@ -1,14 +1,14 @@
 """
 Module containing parameters and settings for Sentinel Hub services
 """
-import copy
+from typing import Any, Optional
 
 from PyQt5.QtCore import QSettings
 
 from .constants import BaseUrl, CrsType, ExtentType, ImageFormat, ImagePriority, ServiceType, TimeType
 
 
-class Settings:
+class Settings(QSettings):
     """A class in charge of all settings. It also handles loading and saving of settings to QGIS settings store"""
 
     # pylint: disable=too-many-instance-attributes
@@ -61,47 +61,43 @@ class Settings:
         "lng_max",
         "download_folder",
     }
-    _auto_save = False
     _CREDENTIAL_STORE_PARAMETERS = {"base_url", "client_id", "client_secret"}
     _STORE_PARAMETERS = _AUTO_SAVE_STORE_PARAMETERS | _CREDENTIAL_STORE_PARAMETERS
 
-    def __init__(self):
+    def __init__(self, path: Optional[str] = None):
+        super().__init__(path)
         self.load_local_settings()
-        self._auto_save = True
 
-    def __setattr__(self, key, value):
+    def __setattr__(self, key: str, value: Any) -> None:
         """Whenever one of the attributes from _AUTO_SAVE_STORE_PARAMETERS is set it is automatically saved to
         QGIS store
         """
-        if self._auto_save and key in self._AUTO_SAVE_STORE_PARAMETERS:
+        if key in self._AUTO_SAVE_STORE_PARAMETERS:
             store_path = self._get_store_path(key)
-            QSettings().setValue(store_path, value)
-
+            self.setValue(store_path, value)
         super().__setattr__(key, value)
 
-    def load_local_settings(self):
-        """Loads settings from QGIS local store"""
-        qsettings = QSettings()
-
+    def load_local_settings(self) -> None:
         for parameter in self._STORE_PARAMETERS:
             store_path = self._get_store_path(parameter)
-            store_value = qsettings.value(store_path)
+            store_value = self.value(store_path)
 
             if store_value is not None:
                 setattr(self, parameter, str(store_value))
 
-    def save_credentials(self):
-        """Saves settings to QGIS local store"""
-        qsettings = QSettings()
+    @staticmethod
+    def _get_store_path(parameter: str) -> str:
+        return f"{Settings._STORE_NAMESPACE}/{parameter}"
 
+    def save_credentials(self) -> None:
+        """Saves settings to QGIS local store"""
         for parameter in self._CREDENTIAL_STORE_PARAMETERS:
             store_path = self._get_store_path(parameter)
-            qsettings.setValue(store_path, getattr(self, parameter))
+            self.setValue(store_path, getattr(self, parameter))
 
-    def _get_store_path(self, parameter_name):
-        """Provides a location of the parameter in the local store"""
-        return f"{self._STORE_NAMESPACE}/{parameter_name}"
-
-    def copy(self):
-        """Provides a copy of a Settings object instance"""
-        return copy.copy(self)
+    def copy(self) -> "Settings":
+        new_settings = Settings(self.fileName())
+        for attr in self.__dict__:
+            if not callable(getattr(self, attr)) and not attr.startswith("_"):
+                setattr(new_settings, attr, getattr(self, attr))
+        return new_settings
--- SentinelHub/tests/conftest.py
+++ SentinelHub/tests/conftest.py
@@ -1,6 +1,7 @@
 """
 Module with global fixtures
 """
+
 import pytest
 
 from .testing_utilities import get_input_folder
@@ -33,3 +34,14 @@ def sh_widget() -> None:
     sh_widget = SentinelHubDockWidget()
     yield sh_widget
     sh_widget.close()
+
+
+@pytest.fixture(scope="session")
+def qsettings(tmp_path_factory) -> None:
+    """Create a temporary file and return a Settings instance"""
+
+    from ..settings import Settings  # noqa: E402
+
+    file_path = tmp_path_factory.mktemp("settings").joinpath("settings")
+    settings = Settings(str(file_path))
+    yield settings
